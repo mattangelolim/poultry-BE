e1e6ebea9fe71a1e2504beffb2c46c27346865ed
mattangeloblim <matt.lim@breddas.com.ph>
2024-02-12 18:36:17 +0800

    commit

--- router/reportsFetcherRouter.js
+++ router/reportsFetcherRouter.js
@@ -2,6 +2,7 @@ const express = require("express")
 const EggReport = require("../models/eggReports")
 const SalesReport = require("../models/eggSalesReport")
 const FlocksReport = require("../models/flockReports")
+const { Op } = require("sequelize")
 const router = express.Router()
 
 router.get("/fetch/egg/approval", async (req, res) => {
@@ -48,5 +49,21 @@ router.get("/fetch/flocks/approval", async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 })
+
+router.get("/fetch/egg/visualization", async (req, res) => {
+    try {
+        const approvalReports = await EggReport.findAll({
+            where: {
+                status: {
+                    [Op.ne]: "pending" // Use Op.ne (not equal) operator to find reports where status is not "pending"
+                }
+            }
+        });
+        res.json(approvalReports);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: error.message });
+    }
+});
 
 module.exports = router
